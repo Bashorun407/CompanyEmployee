d929b425dce0d8f82c912fca7d31184cb30704d5
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-11-09 10:36:19 +0100

    Committing all updated files to local repository

--- MappingProfile.cs
+++ MappingProfile.cs
@@ -9,6 +9,7 @@ namespace CompanyEmployee
         public MappingProfile() 
         {
 
+            //Auto-Mapping for Company class
             /* Auto-mapper for Company classThis CreateMap....is not working as expected
              CreateMap<Company, CompanyDto>()
                  .ForMember(c => c.FullAddress,
@@ -19,8 +20,11 @@ namespace CompanyEmployee
                 .ForMember("FullAddress", 
                 opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
 
-            //Auto-mapper for Employee class
+            CreateMap<CompanyCreationDto, Company>();
+
+            //Auto-mapping for Employee class
             CreateMap<Employee, EmployeeDto>();
+            CreateMap<EmployeeCreationDto, Employee>();
         }
     }
 }
